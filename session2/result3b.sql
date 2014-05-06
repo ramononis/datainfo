5f0c3b744a43c8d3724bc50db59bd12e79a162d3
unknown <ramononis@hotmail.com>
2014-05-06 16:19:07 +0200

    excercise3b.sql

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,10 +1,2 @@
-SELECT p.pid FROM Person p
-WHERE EXISTS(
-	SELECT * FROM Acts a
-	WHERE a.pid = p.pid
-	AND EXISTS (
-		SELECT * FROM Movie m  -- the movies exists and a plays in it
-		WHERE m.mid = a.mid
-		AND m.name = 'Back to the Future'
-	)
-);
+SELECT a.pid FROM Acts a, Movie m
+WHERE (m.name = 'Back to the Future' AND a.mid = m.mid);
